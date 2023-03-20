df474729859e60bd68d41a617bb87af3a800fd33
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2023-03-20 22:57:28 +0000

    Update functionality

--- public/js/swearWordsValidator.js
+++ public/js/swearWordsValidator.js
@@ -1,22 +1,74 @@
-       //---------- Swear Words replacement -----//
-       function validateUsername() {
+$(document).ready(function () {
+      //---------- Swear Words replacement -----//
+        let addPost = $("#createPost")
+        addPost.addEventListener('submit',(e) => {
 
-        <textarea id = "comment-input"></textarea>
-        const  textarea = document.getUserByID('comment-input');
-        let swearWords = /arse | arsehead | arsehole |  ass |  asshole | bastard | bitch |  bloody |  bollocks | brotherfucker | bugger | bullshit | child-fucker | Christ on a bike | Christ on a cracker | cock | cocksucker | crap | cunt | damn | damn it | dick | dickhead | dyke | fatherfucker | frigger | fuck | goddamn | godsdamn | hell | holy shit | horseshit | in shit | Jesus Christ | Jesus fuck | Jesus H. Christ | Jesus Harold Christ | Jesus wept | Jesus, Mary and Joseph | kike |  motherfucker | nigga | nigra | piss | prick | pussy | shit | shit ass | shite | sisterfucker  | slut  | son of a bitch  | son of a whore  | spastic  | sweet Jesus  | turd | twat | wanker/gi ;
-        let userComm1 = textarea.value;
-        let userComm2 = userComm1.replace(swearWords, '*****');
-        document.getElementById('comment-input').value = userComm2;
 
-        if (textarea.value.search(swearWords) <= 0 ){
-            return true;
+       function validateInput() {
+        let description = $("#descrition").val();
+        $("#description").keyup(function () {
+            validateInput();
+          });
+        if (description.search(swearWords)<=0){
+            console.log("Words are replaced !");
+            // return true;
+        } else {
+            let userComm1 = description;
+            description = userComm1.replace(swearWords, '*****');
+            $('#descritpion') = description;
+           
         }
-        else {
-            console.log("Words are not replaced !");
-            return false;
+        let description1 = $('#title').val();
+        $("#title").keyup(function () {
+            validateInput();
+          });
+        if (description1.search(swearWords)<=0){
+            console.log("Words are replaced !");
+            // return true;
+        } else {
+            let userComm1 = description1;
+            description1 = userComm1.replace(swearWords, '*****');
+            $('#title') = description1;
+            
+        }
+        let description2 = $('#post-comments').val();
+        $("#post-comments").keyup(function () {
+            validateInput();
+          });
+        if (description2.search(swearWords)<=0){
+            console.log("Words are replaced !");
+            // return true;
+        } else {
+            let userComm1 = description2;
+            description2 = userComm1.replace(swearWords, '*****');
+            $('#post-comments') = description2;
+            
         }
 
     }
+    validateInput();
+})
+})
+
+     
+        
+        // <textarea id = "description"></textarea>
+        // const  textarea = document.getUserByID('description');
+        // let swearWords = /arse | arsehead | arsehole |  ass |  asshole | bastard | bitch |  bloody |  bollocks | brotherfucker | bugger | bullshit | child-fucker | Christ on a bike | Christ on a cracker | cock | cocksucker | crap | cunt | damn | damn it | dick | dickhead | dyke | fatherfucker | frigger | fuck | goddamn | godsdamn | hell | holy shit | horseshit | in shit | Jesus Christ | Jesus fuck | Jesus H. Christ | Jesus Harold Christ | Jesus wept | Jesus, Mary and Joseph | kike |  motherfucker | nigga | nigra | piss | prick | pussy | shit | shit ass | shite | sisterfucker  | slut  | son of a bitch  | son of a whore  | spastic  | sweet Jesus  | turd | twat | wanker/gi ;
+        // let userComm1 = textarea.value;
+        // let userComm2 = userComm1.replace(swearWords, '*****');
+        // document.getElementById('description').value = userComm2;
+
+        // if (textarea.value.search(swearWords) <= 0 ){
+        //     return true;
+        // }
+        // else {
+        //     console.log("Words are not replaced !");
+        //     return false;
+        // }
+
+//     }
+// })
     
            //---------- Swear Words replacement -----//
     // it('positive test replacement swear words', done =>{
