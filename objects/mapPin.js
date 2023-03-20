5d0bab605bdf7e69fa84bac8474477471d74e1aa
Lach1an <lachlan.mcintyre@hotmail.co.uk>
2023-03-20 19:03:37 +0000

    HTML inside map pins

--- objects/mapPin.js
+++ objects/mapPin.js
@@ -1,18 +1,35 @@
-const { render } = require("ejs");
-
+const ejs = require("ejs");
+const path = require('path');
 class mapPin{
 
     // create user object w/ existing profile
     constructor(postID, post, coords){
-        
         this.postID = postID;
-        this.coords = coords;        
-        // render html
-        this.HTML = render("partials/post/mapPin", {post: post});
-        
+        this.coords = coords;
+        this.HTML = false;
+
+        let temp;
+        // render html -- BASICALLY HELD TOGETHER WITH DUCT TAPE 
+        this.renderHTML(post, function(result){
+
+            temp = result;
+
+        });
+        this.HTML = temp;
     }
 
+    renderHTML(post, callback){
+        ejs.renderFile(path.join(__dirname, "../views/partials/post/postIcon.ejs"), {post: post}, function(err, str){
 
+            if(err){
+                console.log(err);
+                return callback(false);
+            }
+            else{
+                return callback(str); 
+            }
+        });
+    }
 
 
 }
